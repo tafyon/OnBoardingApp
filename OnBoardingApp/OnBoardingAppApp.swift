74a91b72ffad8a6c0f344f5b592d6b86a25bcc08
Tamer Afyon <203502050@ogr.altinbas.edu.tr>
2024-04-28 02:43:26 +0300

    Initial Commit

---
+++ OnBoardingApp/OnBoardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingAppApp.swift
+//  OnBoardingApp
+//
+//  Created by Tamer Afyon on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
